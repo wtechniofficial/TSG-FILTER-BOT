15ee08c900c02c45b8c428ac32c52152340567c1
wtechniofficial <143954770+wtechniofficial@users.noreply.github.com>
2024-01-09 14:57:21 +0530

    Update fotnt_string.py

--- plugins/helper/fotnt_string.py
+++ plugins/helper/fotnt_string.py
@@ -1,6 +1,6 @@
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit @i_m_blogger
+# Subscribe YouTube Channel For Amazing Bot @i_m_blogger
+# Ask Doubt on telegram @i_m_blogger
 
 
 class Fonts:
